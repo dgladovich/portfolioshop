e1ea0a0055900fa972a9cdd109894b1dedfcd6d4
dgladovich <dgladovich@gmail.com>
2018-01-23 21:11:11 +0200

    started working with router

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import Admin from './Admin';
+import CustomAdmin from '../../components/Admin/Admin';
+import router from '../../admin_router';
 
 const title = 'Admin Page';
 const isAdmin = true;
