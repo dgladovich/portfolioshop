0f113ba8ed6ab4ebbf1fcdf8fe130cb0f852b6dc
dgladovich <dgladovich@gmail.com>
2018-01-23 22:08:16 +0200

    plans changed

--- src/admin_router/index.js
+++ src/admin_router/index.js
@@ -4,69 +4,53 @@ import {
     Route,
     Link
 } from 'react-router-dom'
+import { render } from 'react-dom'
 
-const BasicExample = () => (
+
+class App extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+                <ul>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/inbox">Inbox</Link></li>
+                </ul>
+                {this.props.children}
+                <div id='root'></div>
+            </div>
+        )
+    }
+}
+class About extends React.Component{
+    render() {
+        return <h3>About</h3>
+    }
+}
+class Inbox extends React.Component{
+    render() {
+        return (
+            <div>
+                <h2>Inbox</h2>
+                {this.props.children || "Welcome to your Inbox"}
+            </div>
+        )
+    }
+}
+class Message extends React.Component{
+    render() {
+        return <h3>Message {this.props.params.id}</h3>
+    }
+}
+
+
+render((
     <Router>
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/topics">Topics</Link></li>
-            </ul>
-
-            <hr/>
-
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/topics" component={Topics}/>
-        </div>
+        <Route path="/" component={App}>
+            <Route path="about" component={About} />
+            <Route path="inbox" component={Inbox}>
+                <Route path="messages/:id" component={Message} />
+            </Route>
+        </Route>
     </Router>
-)
-
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
-
-const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
-)
-
-const Topics = ({ match }) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>
-                    Rendering with React
-                </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>
-                    Components
-                </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>
-                    Props v. State
-                </Link>
-            </li>
-        </ul>
-
-        <Route path={`${match.url}/:topicId`} component={Topic}/>
-        <Route exact path={match.url} render={() => (
-            <h3>Please select a topic.</h3>
-        )}/>
-    </div>
-)
-
-const Topic = ({ match }) => (
-    <div>
-        <h3>{match.params.topicId}</h3>
-    </div>
-)
-
-export default BasicExample
+), document.getElementById('root'))
--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import Admin from './Admin';
-import CustomAdmin from '../../components/Admin/Admin';
-import router from '../../admin_router';
 
 const title = 'Admin Page';
 const isAdmin = true;
